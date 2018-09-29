621679c69008f7537b9222ab910fb79686ac5287
m-rwash <m.rwash93@yahoo.com>
2018-09-29 16:41:57 +0200

    move custome colors to UIColor extension

---
+++ TopAnime/Anime.swift
@@ -0,0 +1,9 @@
+//
+//  Anime.swift
+//  TopAnime
+//
+//  Created by Mohamed Rwash on 9/29/18.
+//  Copyright © 2018 mrwash. All rights reserved.
+//
+
+import Foundation
---
+++ TopAnime/UIColor+Theme.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor+Theme.swift
+//  TopAnime
+//
+//  Created by Mohamed Rwash on 9/29/18.
+//  Copyright © 2018 mrwash. All rights reserved.
+//
+import UIKit
+
+extension UIColor {
+    static let darkOrangeColor = UIColor(red: 1, green: 132/255, blue: 34/255, alpha: 1)
+    static let pinkColor = UIColor(red: 250/255, green: 101/255, blue: 96/255, alpha: 1)
+    static let lightBlueColor = UIColor(red: 0, green: 180/255, blue: 251/255, alpha: 1)
+    static let darkBlueColor = UIColor(red: 0, green: 26/255, blue: 65/255, alpha: 1)
+    static let whiteBlueColor = UIColor(red: 218/255, green: 235/255, blue: 243/255, alpha: 1)
+
+}
